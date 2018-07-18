3b172be1b37fea097fd22ec678810aaed7d7f0c5
Carlos Carballo <ballokalitos@gmail.com>
2018-07-18 05:00:35 -0500

    formulario obteniendo info x template1

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styles: []
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent {
+
+  usuario:Object = {
+    nombre: "Fernando",
+    apellido: "Herrera",
+    correo: ""
+  }
 
   constructor() { }
 
-  ngOnInit() {
+  guardar(forma:NgForm){
+
+    console.log( "ngForm", forma);
+    console.log( "Valor forma", forma.value);
+
+    console.log( "Usuario", this.usuario);
+
   }
 
 }
