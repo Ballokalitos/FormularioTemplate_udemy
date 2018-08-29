f12a5bee5e456bb78c68d121638444c3704bbdd5
Carlos Carballo <ballokalitos@gmail.com>
2018-08-29 10:32:06 -0500

    Data_aproximacion jugando con arrays

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -9,22 +9,54 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class DataComponent {
    forma:FormGroup;
 
+   usuario:Object = {
+      nombrecompleto: {
+        nombre:"Carlos",
+        apellido:"carballo"
+      },
+      correo: "carlos.carballo@dafqroo.gpb.mx",
+      // pasatiempos: ["Correr","Dormir","Correr"]
+   }
+
   constructor() {
 
+    console.log(this.usuario);
+
     this.forma = new FormGroup({
-     'nombre': new FormControl('Fernando'),
-     'apellido': new FormControl(),
-     'correo': new FormControl()
+      'nombrecompleto': new FormGroup({
+         'nombre': new FormControl('' ,  [
+           Validators.required,
+           Validators.minLength(3)
+         ]),
+          'apellido': new FormControl('' , Validators.required )
+          }),
+           'correo': new FormControl('' ,   [
+                                      Validators.required,
+                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+                                    ]  ),
+            'pasatiempos': new FormArray([
+              new FormControl('Correr',Validators.required)
+            ])                        
    })
+   // this.forma.setValue( this.usuario);
 
-
-
-
+   }
+   agregarPasatiempo(){
+     (<FormArray>this.forma.controls['pasatiempos']).push(
+       new FormControl('Dormir', Validators.required)
+     )
    }
 
    guardarCambios(){
       console.log( this.forma.value);
       console.log( this.forma);
+      this.forma.reset({
+        nombrecompleto:{
+          nombre: "",
+          apellido: ""
+        },
+        correo:""
+      });
    }
 
 
